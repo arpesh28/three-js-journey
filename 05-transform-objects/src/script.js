15babf438b7450877b6ce4efc02ed9479201a826
arpesh28 <arpeshgadekar@gmail.com>
2023-06-17 09:18:34 +0530

    added orbit control

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,6 +1,19 @@
 import * as THREE from "three";
 import gsap from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
+/**
+ * Cursor Movement
+ */
+const cursor = {
+  x: 0,
+  y: 0,
+};
+//  Mouse move event listener
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+});
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -46,17 +59,25 @@ scene.add(axesHelper);
  * Sizes
  */
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 /**
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100);
 camera.position.z = 3;
 scene.add(camera);
-// camera.lookAt(mesh.position); // Look at a particular position
+// camera.position.set(2, 2, 2);
+camera.lookAt(mesh.position); // Look at a particular position
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
@@ -70,18 +91,26 @@ renderer.setSize(sizes.width, sizes.height);
  * Animations
  */
 
-gsap.to(mesh.position, { x: 2, duration: 1, delay: 1 });
+// gsap.to(mesh.position, { x: 2, duration: 1, delay: 1 });
 
 const clock = new THREE.Clock();
 const tick = () => {
-  const time = clock.getElapsedTime();
+  //   const time = clock.getElapsedTime();
   //    Update Objects
-  //   mesh.rotation.y = time * Math.PI * 2; //   Math.PI is used for half rotation
-  //   mesh.position.y = Math.sin(time); //   Sin(x) is a wave starting from 0
-  //   mesh.position.z = Math.cos(time); //  Cos(x) is a wave starting from 1
-  //   mesh.position.x = Math.cos(time);
+  //   mesh.rotation.x = cursor.y; //   Math.PI is used for half rotation
+  //   mesh.rotation.y = cursor.x; //   Math.PI is used for half rotation
+  //   mesh.position.x = cursor.x * 2;
+  //   mesh.position.y = -cursor.y * 2;
+  // camera.position.x = Math.cos(cursor.x * Math.PI * 2) * 2;
+  // camera.position.z = Math.sin(cursor.x * Math.PI * 2) * 2;
+  // camera.position.y = cursor.y * 5;
+  // camera.lookAt(mesh.position);
+  // mesh.position.y = Math.sin(time); //   Sin(x) is a wave starting from 0
+  // mesh.position.z = Math.cos(time); //  Cos(x) is a wave starting from 1
+  // mesh.position.x = Math.cos(time);
 
   //  Render
+  controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
