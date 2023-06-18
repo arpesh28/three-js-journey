c7ca293b573b9e08f6092e23959d970b838296b2
arpesh28 <arpeshgadekar@gmail.com>
2023-06-18 09:58:04 +0530

    Texture Lesson completed

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -25,7 +25,7 @@ loadingManager.onError = () => {
   console.log("onError");
 };
 const textureLoader = new THREE.TextureLoader(loadingManager);
-const colorTexture = textureLoader.load("/textures/door/color.jpg");
+const colorTexture = textureLoader.load("/textures/minecraft.png");
 const alphaTexture = textureLoader.load("/textures/door/alpha.jpg");
 const heightTexture = textureLoader.load("/textures/door/height.jpg");
 const normalTexture = textureLoader.load("/textures/door/normal.jpg");
@@ -36,11 +36,20 @@ const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
 const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
 
 // colorTexture.repeat.x = 2;
-// colorTexture.repeat.y = 2;
-colorTexture.wrapS = THREE.MirroredRepeatWrapping;
-colorTexture.wrapT = THREE.MirroredRepeatWrapping;
-colorTexture.offset.x = 0.5;
-colorTexture.offset.y = 0.5;
+// colorTexture.repeat.y = 3;
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping;
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping;
+// colorTexture.offset.x = 0.5;
+// colorTexture.offset.y = 0.5;
+// colorTexture.rotation = Math.PI * 0.25;
+// colorTexture.center.x = 0.5;
+// colorTexture.center.y = 0.5;
+
+/**====>>MipMapping & Filters */
+
+colorTexture.magFilter = THREE.NearestFilter;
+
+/**====>>End of  MimMapping & Filters */
 
 /**
  * End of Textures
@@ -103,6 +112,17 @@ scene.add(camera);
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+controls.autoRotate = true;
+
+const mouse = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = event.clientX / window.innerWidth - 0.5;
+  mouse.y = event.clientY / window.innerHeight - 0.5;
+});
 
 /**
  * Renderer
@@ -120,7 +140,6 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
   // Update controls
   controls.update();
 
