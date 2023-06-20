1fc3e7328b70899a2f50d1b0665e97aec95e8c58
arpesh28 <arpeshgadekar@gmail.com>
2023-06-20 09:00:47 +0530

    haunted house

--- Project-17-Haunted-House/src/script.js
+++ Project-17-Haunted-House/src/script.js
@@ -166,6 +166,7 @@ for (let i = 0; i < 50; i++) {
   grave.position.set(x, 0.3, z);
   grave.rotation.y = (Math.random() - 0.5) * 1;
   grave.rotation.z = (Math.random() - 0.5) * 0.4;
+  grave.castShadow = true;
   graves.add(grave);
 }
 
@@ -215,6 +216,20 @@ house.add(doorLight);
 
 /**
  * ===>> End Lights
+ */
+
+/**
+ * ===>> Start Ghosts
+ */
+
+const ghost1 = new THREE.PointLight("#ff00ff", 2, 3);
+const ghost2 = new THREE.PointLight("#00ffff", 2, 3);
+const ghost3 = new THREE.PointLight("#ffff00", 2, 3);
+
+scene.add(ghost1, ghost2, ghost3);
+
+/**
+ * ===>> End Ghosts
  */
 
 /**
@@ -269,12 +284,66 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setClearColor("#262837");
 
 /**
+ * ===>> Shadows
+ */
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+
+moonLight.castShadow = true;
+doorLight.castShadow = true;
+ghost1.castShadow = true;
+ghost2.castShadow = true;
+ghost3.castShadow = true;
+walls.castShadow = true;
+bush1.castShadow = true;
+bush2.castShadow = true;
+bush3.castShadow = true;
+bush4.castShadow = true;
+doorLight.castShadow = true;
+
+floor.receiveShadow = true;
+
+doorLight.shadow.mapSize.width = 256;
+doorLight.shadow.mapSize.height = 256;
+doorLight.shadow.camera.far = 7;
+
+ghost1.shadow.mapSize.width = 256;
+ghost1.shadow.mapSize.height = 256;
+ghost1.shadow.camera.far = 7;
+
+ghost2.shadow.mapSize.width = 256;
+ghost2.shadow.mapSize.height = 256;
+ghost2.shadow.camera.far = 7;
+
+ghost3.shadow.mapSize.width = 256;
+ghost3.shadow.mapSize.height = 256;
+ghost3.shadow.camera.far = 7;
+
+/**
  * Animate
  */
 const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  //  Update Ghosts
+  const ghost1Angle = elapsedTime * 0.5;
+  ghost1.position.x = Math.cos(ghost1Angle) * 4;
+  ghost1.position.z = Math.sin(ghost1Angle) * 4;
+  ghost1.position.y = Math.cos(elapsedTime) * 3;
+
+  const ghost2Angle = -elapsedTime * 0.32;
+  ghost2.position.x = Math.cos(ghost2Angle) * 5;
+  ghost2.position.z = Math.sin(ghost2Angle) * 5;
+  ghost2.position.y = Math.cos(elapsedTime) * 4 + Math.cos(elapsedTime) * 2.5;
+
+  const ghost3Angle = -elapsedTime * 0.18;
+  ghost3.position.x =
+    Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32));
+  ghost3.position.z =
+    Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32));
+  ghost3.position.y = Math.cos(elapsedTime) * 5 + Math.cos(elapsedTime) * 2;
 
   // Update controls
   controls.update();
